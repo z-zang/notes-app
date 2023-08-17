85c5a43eb2e2c816325406bacf70403fcdb0ddc6
Zichao Zang <zang.code@gmail.com>
2023-08-17 20:16:17 +0100

    refactor: change app state from useState to useReducer

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 type Props = {}
 
 const Note = (props: Props) => {
-    const { notebooks, setNotebooks } = useNotebooksContext()
+    const { notebooks, dispatch } = useNotebooksContext()
     const { notebookId, noteId } = useParams();
     const notebook = notebooks.find(notebook => notebook.id.toString() === notebookId)!
     const note = notebook.notes.find(note => note.id.toString() === noteId)!
@@ -13,21 +13,14 @@ const Note = (props: Props) => {
     // console.log('note', note)
 
     const handleTextChange = (updatedBody: string) => {
-
-        const updatedNote = {
-            ...note,
-            body: updatedBody
-        }
-        const remainingNotes = notebook.notes.filter(note => note.id.toString() !== noteId)!
-
-        const updatedNotebook = {
-            ...notebook,
-            notes: [updatedNote, ...remainingNotes]
-        }
-        const remainingNotebooks = notebooks.filter(notebook => notebook.id.toString() !== notebookId)!
-        const updatedNotebooks = [updatedNotebook, ...remainingNotebooks]
-
-        setNotebooks(updatedNotebooks)
+        dispatch({
+            type: 'UPDATE_NOTE_BODY',
+            payload: {
+                notebookId: Number(notebookId),
+                noteId: Number(noteId),
+                body: updatedBody
+            }
+        })
     }
 
     return (
--- src/hooks/NotebooksContext.tsx
+++ src/hooks/NotebooksContext.tsx
@@ -1,27 +1,62 @@
-import { useState, useContext, createContext, Dispatch, SetStateAction, ReactNode } from "react";
-import { Notebook } from "../types/Notebook";
+/* eslint-disable no-case-declarations */
+
+import { useState, useContext, useReducer, createContext, Dispatch, SetStateAction, ReactNode } from "react";
+import { Note, Notebook } from "../types/Notebook";
 import { notebooksData } from "./notebookData";
 
 type NotebooksContextType = {
     notebooks: Notebook[],
-    setNotebooks: Dispatch<SetStateAction<Notebook[]>>
+    dispatch: Dispatch<NotebookAction>
 }
-
-const NotebooksContext = createContext<NotebooksContextType>({
-    notebooks: [],
-    setNotebooks: () => { }
-})
 
 type NotebooksContextProviderPropsType = {
     children: ReactNode[] | ReactNode
 }
 
+const NotebooksContext = createContext<NotebooksContextType>({
+    notebooks: [],
+    dispatch: () => { }
+})
+
+
+type UPDATE_NOTE_BODY = {
+    type: 'UPDATE_NOTE_BODY',
+    payload: {
+        notebookId: number,
+        noteId: number,
+        body: string
+    }
+};
+type NotebookAction = UPDATE_NOTE_BODY
+
 const NotebooksContextProvider = ({ children }: NotebooksContextProviderPropsType) => {
     // todo: change to useReducer?
-    const [notebooks, setNotebooks] = useState<Notebook[]>(notebooksData)
+    const notebookReducer = (notebooks: Notebook[], action: NotebookAction) => {
+        switch (action.type) {
+            case 'UPDATE_NOTE_BODY':
+                const notebook = notebooks.find(notebook => notebook.id === action.payload.notebookId)!
+                const note = notebook.notes.find(note => note.id === action.payload.noteId)!
+
+                const updatedNote = {
+                    ...note,
+                    body: action.payload.body
+                }
+                const remainingNotes = notebook.notes.filter(note => note.id !== action.payload.noteId)!
+
+                const updatedNotebook = {
+                    ...notebook,
+                    notes: [updatedNote, ...remainingNotes]
+                }
+                const remainingNotebooks = notebooks.filter(notebook => notebook.id !== action.payload.notebookId)!
+                const updatedNotebooks = [updatedNotebook, ...remainingNotebooks]
+                return updatedNotebooks
+        }
+    }
+
+    const [notebooks, dispatch] = useReducer(notebookReducer, notebooksData)
 
     return (
-        <NotebooksContext.Provider value={{ notebooks, setNotebooks }}>
+        <NotebooksContext.Provider value={{ notebooks, dispatch }}>
             {children}
         </NotebooksContext.Provider>
     )
